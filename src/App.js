187a40fcbf173896c1470662ca9e7a72ad4d5298
Kudzai <alpha.kudzai@gmail.com>
2021-12-16 09:52:09 +0200

    added the images into src to control size

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import { createStore } from 'redux'
 import { CSSTransition } from "react-transition-group"
 import { useState } from 'react'
 import klogo from './klogo.svg'
+import drumMachine from './drummachine.jpg'
+import markdownP from './markdown.jpg'
+import oldPortfolio from './portfoliosite.jpg'
+import productLpage from './productpage.jpg'
+import quoteMachine from './quotemachine.jpg'
+import surveyForm from './surveyform.jpg'
+import techDocSite from './techdocsite.jpg'
+import timerSite from './timer.jpg'
+import tributePage from './tributesite.jpg'
 import phone from './phone.png'
 import emailImg from './email.png'
 import github from './github.png'
@@ -26,7 +35,7 @@ const intialState = {
   bestWorkLinks: ['https://codepen.io/alp-kudzai/pen/XWgNLPq', 'https://codepen.io/alp-kudzai/pen/NWgRgvp', 'https://codepen.io/alp-kudzai/pen/MWopxXj', 'https://codepen.io/alp-kudzai/pen/XWaPrpJ'],
   bestWorkTitle: ['Product/Services Landing Page', 'General Form', 'Old Portfolio Site', '25 + 5 Countdown Timer'],
   bestWorkCurrent: 'Product/Services Landing Page',
-  bestWork: ['https://db5pap001files.storage.live.com/y4mdOalpR0KHHZaIO4G-i3_OelSKJqcZo8Bctey9TsLs-xzCDbBgLHOrGmzPFk8SJ_sgquV-YsxaOIa8SaP2YKfQL8rosvpOIS_feIqgT490OboeTrSovALNZWtzvXtXjenxSIVfbQXoj-RPl33WDtDDNVwygZs9ukg7BkqzxwNaNCHSAHgYADtu3BSWgzTtwN8?width=660&height=291&cropmode=none','https://db5pap001files.storage.live.com/y4mtkzJHzq17mOS0YbW_vQNoglEmKXGXYd71qSWI458uJxuPXQwX75VHUdj_ogcu7cDIs1nNdAjzO4zBMbzn_5dG9Jbsk9wXecekuhunsKS8cY5PUAQVw3-OU5JU7qaBetJtA-IrPzalZZI-EyDhMdAjs8dJ7mIpiDxIoQNORvWpwrGLmeL7GnD8arh8e-4m1NZ?width=660&height=291&cropmode=none','https://db5pap001files.storage.live.com/y4mp2HpVf9jj1evU8fZL3oI_WGuW_kBPUbZYXEn0KXlFsxdezhXt4zpL4-cph8g0btG22jr1PeuXCAAj87IY6zusHXszB8Q4Yk9QCC9osZ6UbOuqOX7g06D1R4SLNax59CGrFVT8KT1XBMGnpVTiRG0FubWlilYsJIFVi4op2qqi9CfSf14K9vEWk-SREeqBGaO?width=660&height=292&cropmode=none','https://db5pap001files.storage.live.com/y4mwFqVHS-62fzz6ZyRuSrNWjOW94fQ35NBv5qDLHGuv1vTrSr8kbe8LR6eWtmIOrMSiz_rXS2w8egmM1NHneff9ezIUlmg8xpgOcj4EyTLG_jRBRCkK7zcWuxWprLoAZDNK0fqy0GOfGcO6vXcKPlCvZBnUYREHrBjAgaw4azc2ErMk2-HccajQERPEr_ooM5X?width=660&height=289&cropmode=none'],
+  bestWork: [productLpage,surveyForm,oldPortfolio,timerSite],
 }
 
 // const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup
@@ -191,16 +200,16 @@ let glowStyle = {
                       <img id='mylogo' width='128' height='128' src={klogo} alt='klogo' id='klogo' onClick={handleAbout}/>
                   </li>
                   <li>
-                      <button onClick={handleAbout} style={navGlow(state, 'about')} id='Babout'>About</button>
+                      <button className='nav-button' onClick={handleAbout} style={navGlow(state, 'about')} id='Babout'>About</button>
                   </li>
                   <li>
-                      <button onClick={handleBestWork} style={navGlow(state, 'bestWork')} id='BbestWork'>Best Work</button>
+                      <button className='nav-button' onClick={handleBestWork} style={navGlow(state, 'bestWork')} id='BbestWork'>Best Work</button>
                   </li>
                   <li>
-                      <button onClick={handleWork} style={navGlow(state, 'work')} id='Bwork'>Work</button>
+                      <button className='nav-button' onClick={handleWork} style={navGlow(state, 'work')} id='Bwork'>Work</button>
                   </li>
                   <li>
-                      <button onClick={handleResume} style={navGlow(state, 'resume')} id='Bresume'>Resum&egrave;</button>
+                      <button className='nav-button' onClick={handleResume} style={navGlow(state, 'resume')} id='Bresume'>Resum&egrave;</button>
                   </li>
               </ul>   
       </div>
@@ -300,7 +309,7 @@ const Work = () => {
       <div id='work'>
           <div className='workItem'>
               {/* Quote Machine */}
-              <img className='proj-image' alt='Quote Machine' src='https://db5pap001files.storage.live.com/y4mS0s1w-_8ckx0GIIXr_JRVW81M6amXo_luJ2oONaMH_-dGYQxLsZVMHQoOY3897oV2NehoSeewW21g1q71Gip0tOjx7AqJ7vg1NLVscFB1Ag6JO5gQuSWTj8i36fEGE9O394Lim7Rmgw5MYyTo0Vm_4r3WBg8WNRQS2pmh00Nr2S1Mfs0rxhO52Q79mIkHVaq?width=660&height=289&cropmode=none'/>
+              <img className='proj-image' alt='Quote Machine' src={quoteMachine}/>
               <div className='workItem-text'>
                   <h3><a target='_blank' rel='noreferrer' id='workItem-link' href='https://codepen.io/alp-kudzai/pen/eYRLxQL'>Quote Machine</a></h3>
               </div>
@@ -308,7 +317,7 @@ const Work = () => {
           </div>
           <div className='workItem'>
               {/* Drum Machine */}
-              <img className='proj-image' src='https://db5pap001files.storage.live.com/y4mu0YSO7APQKzjYoM1jBuic2zOVcWUNpaGv3Ud3y2vmFELeJPs7yqCkrqbrzHzbFF_4w7Jp6GYkq0nSxH8FYxEr9KA0lpCyTbWvQoyenvSpDhrH_fV4ZUzjXd-3ODGMMmC2l4RngXjCBfQ-tBWGZOfRPP61rUrMwlmR_TSzzjrbtnb3FE2nQBduzBAs8PO41LV?width=660&height=289&cropmode=none' alt='Drum Machine'/>
+              <img className='proj-image' src={drumMachine} alt='Drum Machine'/>
               <div className='workItem-text'>
                   <h3><a target='_blank' rel='noreferrer' id='workItem-link' href='https://codepen.io/alp-kudzai/pen/LYjVjab'>Drum Machine</a></h3>
               </div> 
@@ -316,7 +325,7 @@ const Work = () => {
           </div>
           <div className='workItem'>
               {/* Tribute page   */}
-              <img className='proj-image' alt='Tribute Page' src='https://db5pap001files.storage.live.com/y4mnbrkil85e35InyUTl_kpRhq65EVrtKVKGMmCCqkmjjQP7FFpGrPtyz2dF6TJX84zGCTSPBDORyNI3hbNCbzF7vQJE7vyng9Jel0KkpQ_upJdMXUEy4mjgHRxJBTbeVjduvH9QabR0i18Mt2V61oK9BtXPage4O_oqqUYrMDcgvHoFF69W9cBItiCNIc24bZS?width=660&height=289&cropmode=none'/>
+              <img className='proj-image' alt='Tribute Page' src={tributePage}/>
               <div className='workItem-text'>
                   <h3><a target='_blank' rel='noreferrer' id='workItem-link' href='https://codepen.io/alp-kudzai/pen/YzQGqaR'>Tribute Page</a></h3>
               </div> 
@@ -324,7 +333,7 @@ const Work = () => {
           </div>
           <div className='workItem'>
               {/* Technical documentation */}
-              <img className='proj-image' src='https://db5pap001files.storage.live.com/y4m8TBJFzDcD_Xrr5LdbdMr45TGDG7G0mlcUcxXDSVXCYOi3VnOt8sKUFQWOLG3mTZw396x6eZ7flF0R0IHEW14Jr0uFvv4MenqY4v4fYbfOuIQKoVAecAqVHAyKoM2VfEzqs4FGCspvDm68AyYhZ-0hA_cTwbv2kQtvF3TA4NPKBQhQYfkJK_u056V3P37P5S_?width=660&height=289&cropmode=none' alt='Techinal Doc Site'/>
+              <img className='proj-image' src={techDocSite} alt='Techinal Doc Site'/>
               <div className='workItem-text'>
                   <h3><a target='_blank' rel='noreferrer' id='workItem-link' href='https://codepen.io/alp-kudzai/pen/OJgpbyO'>Technical Documentation Site</a></h3>
               </div> 
@@ -332,7 +341,7 @@ const Work = () => {
           </div>
           <div className='workItem'>
               {/* MarkDown Previewer */}
-              <img className='proj-image' alt='Markdown Previewer' src='https://db5pap001files.storage.live.com/y4mQACgPZAQvJWj8XVS_sLxojU3i348jdB54pezIL4uzWi2vc6Mb5JPEamGo0Ug4CnbHlz8hkE-L9jC9zTNFlS6tKRVbM3eKA46h0g9BvL7dV7hNcvrpgQrVAxmzvi9exRFOjGmQC04mBzW9dy98FQ9FQlXtC7IvJpGwds7RikiBxm9Jy5Q5sdNbJo5yG61Furn?width=660&height=289&cropmode=none'/> 
+              <img className='proj-image' alt='Markdown Previewer' src={markdownP}/> 
               <div className='workItem-text'>
                   <h3><a target='_blank' rel='noreferrer' id='workItem-link' href='https://codepen.io/alp-kudzai/pen/MWvWyOo'>Markdown Previewer</a></h3>
               </div> 
@@ -361,28 +370,28 @@ const Resume = () => {
                 </div>
                 <div id='personal-info'>
                     <div id='location'>
-                        <img alt='' src={locationIcon} width='40px' height='40px'/>
+                        <img className='icon-link' alt='' src={locationIcon} />
                         <p>Johannesburg, South Africa</p>
                     </div>
                     <div id='contact-info'>
                         <div id='contact'>
                             <div id='phone-num'>
-                                <img alt='' src={phone} width='40px' height='40px'/>
+                                <img className='icon-link' alt='' src={phone} />
                                 <p>+27 81 526 3026</p>
                             </div>
                             <div id='email-info'>
-                                <img alt='' src={emailImg} width='40px' height='40px'/>
+                                <img className='icon-link' alt='' src={emailImg} />
                                 <p>alpha.kudzai@gmail.com</p>
                             </div>
                             <div id='social-media'>
                                 <a className='resume-link' rel='noreferrer' target='_blank' href='https://www.linkedin.com/in/kudzai-matsika-117698182/'>
-                                    <img className='icon-link' alt='' src={linkedin} width='40px' height='40px'/>
+                                    <img className='icon-link' alt='' src={linkedin} />
                                 </a>
                                 <a className='resume-link' rel='noreferrer' target='_blank' href='https://github.com/alp-kudzai'>
-                                    <img className='icon-link' alt='' src={github} width='40px' height='40px'/>
+                                    <img className='icon-link' alt='' src={github} />
                                 </a>
                                 <a className='resume-link' rel='noreferrer' target='_blank' href='https://twitter.com/KudzaiAlpha'>
-                                    <img className='icon-link' alt='' src={twitter} width='40px' height='40px'/>
+                                    <img className='icon-link' alt='' src={twitter} />
                                 </a>
                             </div>
                         </div>
